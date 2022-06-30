4cb7f8bd17e8622a5153019a9dc7b8ff43f1c407
shixianyan <2723180640@qq.com>
2022-06-30 16:51:20 +0800

    first commit

---
+++ src/main/java/sxy/com/git/GitTest.java
@@ -0,0 +1,11 @@
+package sxy.com.git;
+
+/**
+ * @author SXY
+ * @date 2022/6/30 - 下午4:48
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("dajkwhdlawbfdjlqwh");
+    }
+}
