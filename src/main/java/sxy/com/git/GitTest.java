690ff961f3806430fe9aa96ec5578991fcd08895
shixianyan <2723180640@qq.com>
2022-06-30 16:54:16 +0800

    third commit

--- src/main/java/sxy/com/git/GitTest.java
+++ src/main/java/sxy/com/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("dajkwhdlawbfdjlqwh");
         System.out.println("这dajkwhdlawbfdjlqwh");
+        System.out.println("是dajkwhdlawbfdjlqwh");
     }
 }
